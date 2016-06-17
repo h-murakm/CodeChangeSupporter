d3021cb78de4b75e3f4d11491416cc242f70cc89
h-murakm <h-murakm@h-murakm-PC>
2016-06-18 01:17:11 +0900

    implement CloneDetectionMain.detectClones()

--- src/clone/detection/main/CloneDetectionMain.java
+++ src/clone/detection/main/CloneDetectionMain.java
@@ -3,12 +3,12 @@ package clone.detection.main;
 public class CloneDetectionMain {
 
 	public static void detectClones(String targetProject, int minimalTokenLength, String fileName, int selectedLine, String resultFile, String workingDirectory) {
-//		System.out.println(targetProject);
-//		System.out.println(minimalTokenLength);
-//		System.out.println(fileName);
-//		System.out.println(selectedLine);
-//		System.out.println(resultFile);
-//		System.out.println(workingDirectory);
+		System.out.println(targetProject);
+		System.out.println(minimalTokenLength);
+		System.out.println(fileName);
+		System.out.println(selectedLine);
+		System.out.println(resultFile);
+		System.out.println(workingDirectory);
 	}
 
 }
